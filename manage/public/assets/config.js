0e22fc023df5b89a2aed1fd0437d5efcaafa0d93
徐美红 <l>
2022-03-14 14:35:07 +0800

    pc/manage 更新为上海环境cas认证地址

--- manage/public/assets/config.js
+++ manage/public/assets/config.js
@@ -1,4 +1,4 @@
 const serverConfig = {
     BACKEND_BASE_URL: "https://school-bus.dev2.supwisdom.com",
-    AUTH_CAS: "https://cas-test.paas.newcapec.cn/cas", // CAS
+    AUTH_CAS: "https://cas.dev2.supwisdom.com/cas", // CAS
 };
--- pc/public/assets/config.js
+++ pc/public/assets/config.js
@@ -1,5 +1,5 @@
 const serverConfig = {
     BACKEND_BASE_URL: "https://school-bus.dev2.supwisdom.com",
-    AUTH_CAS: "http://cas-test.paas.newcapec.cn/cas", // CAS
+    AUTH_CAS: "https://cas.dev2.supwisdom.com/cas", // CAS
     LayoutUrl: 'https://portal-service1.paas.newcapec.cn' //布局请求地址
 };
